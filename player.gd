0f0eae38beafe41aafd7e17909c98be2ab9dff13
jazyt3871 <jasperschepers12@gmail.com>
2026-02-24 14:46:57 +0100

    downsmash

--- player.gd
+++ player.gd
@@ -1,14 +1,12 @@
 extends CharacterBody2D
 
 @export var speed = 180.0
+@export var downjump_precision = 100
+@export var downjump_velocity = 3.0
 @export var jump_velocity = -400.0
-@export var double_jump_velocity = -350.0 # Often slightly lower for feel
-
-# Ground physics
+@export var double_jump_velocity = -350.0
 @export var ground_acceleration = 2500.0
 @export var ground_friction = 3000.0
-
-# Air physics
 @export var air_acceleration = 600.0
 @export var air_friction = 200.0 
 
@@ -30,6 +28,12 @@ func _physics_process(delta):
 		elif can_double_jump:
 			velocity.y = double_jump_velocity
 			can_double_jump = false # Use it up!
+			
+	if Input.is_action_just_pressed("down"):
+		if abs(velocity.y) < 100:
+			var precision = downjump_precision - abs(velocity.y)
+			velocity.y = downjump_velocity * precision * 2
+
 
 	# 3. Determine physics values based on state
 	var accel = ground_acceleration if is_on_floor() else air_acceleration
