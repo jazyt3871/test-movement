37ddfda8954c658818d4bbf330396539f9a7744b
jazyt3871 <jasperschepers12@gmail.com>
2026-02-24 11:18:45 +0100

    movement

---
+++ player.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+@export var speed = 180.0
+@export var jump_velocity = -400.0
+@export var double_jump_velocity = -350.0 # Often slightly lower for feel
+
+# Ground physics
+@export var ground_acceleration = 2500.0
+@export var ground_friction = 3000.0
+
+# Air physics
+@export var air_acceleration = 600.0
+@export var air_friction = 200.0 
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var can_double_jump = false
+
+func _physics_process(delta):
+	# 1. Apply Gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		# Reset double jump when we touch the ground
+		can_double_jump = true
+
+	# 2. Handle Jump & Double Jump
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_velocity
+		elif can_double_jump:
+			velocity.y = double_jump_velocity
+			can_double_jump = false # Use it up!
+
+	# 3. Determine physics values based on state
+	var accel = ground_acceleration if is_on_floor() else air_acceleration
+	var frict = ground_friction if is_on_floor() else air_friction
+
+	# 4. Handle Horizontal Movement
+	var direction = Input.get_axis("left", "right")
+	
+	if direction != 0:
+		velocity.x = move_toward(velocity.x, direction * speed, accel * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, frict * delta)
+
+	move_and_slide()
